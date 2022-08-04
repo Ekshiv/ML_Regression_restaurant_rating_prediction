3cdfc76cd3ace386625e4b3b30d032532507232e
ekshiv <ekshiv1896@gmail.com>
2022-08-04 21:07:23 +0530

    exception done

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import sys
 from flask import Flask
 from restaurant.logger import logging
+from restaurant.exception import RestaurantException
 
 app=Flask(__name__)
 
 @app.route('/') # never give space after this line
 def index():
+    try:
+        raise Exception("We are testing Exception module in our project")
+    except Exception as e:
+        restaurant=RestaurantException(e,sys)
+        logging.info(restaurant.error_message)
     logging.info("we are testing logging module")
     return "HEY I'm STARTING NEW PROJECT on MACHINE LEARNING"
 
