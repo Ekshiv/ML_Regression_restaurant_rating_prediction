ff9508ee309a3812ef783f07607a914c5967667e
ekshiv <ekshiv1896@gmail.com>
2022-08-04 12:59:44 +0530

    logging done and folder added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from restaurant.logger import logging
 
 app=Flask(__name__)
 
 @app.route('/') # never give space after this line
 def index():
+    logging.info("we are testing logging module")
     return "HEY I'm STARTING NEW PROJECT on MACHINE LEARNING"
 
 if __name__=='__main__':
